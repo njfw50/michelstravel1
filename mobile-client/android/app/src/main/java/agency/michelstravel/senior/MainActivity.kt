e0b77e46668c71b319699dce572ee8ffe0461939
njfw50 <njfw50@gmail.com>
2026-03-29 21:59:25 -0400

    Make Android app package definitive

---
+++ mobile-client/android/app/src/main/java/agency/michelstravel/senior/MainActivity.kt
@@ -0,0 +1,14 @@
+package agency.michelstravel.senior
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  override fun getMainComponentName(): String = "MichelsTravel"
+
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
