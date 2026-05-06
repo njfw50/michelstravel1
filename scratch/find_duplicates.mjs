54eec7a6bafdce05509347c0cdaf21bb3f31351c
njfw50 <njfw50@gmail.com>
2026-05-05 23:39:54 -0400

    chore: cleanup scratch scripts and finalize production hardening

--- scratch/find_duplicates.mjs
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-const content = fs.readFileSync('../michelstravel1/client/src/locales/pt.json', 'utf8');
-let pos = -1;
-while ((pos = content.indexOf('"home":', pos + 1)) !== -1) {
-    const line = content.substring(0, pos).split('\n').length;
-    console.log('Found "home": at line:', line);
-}
